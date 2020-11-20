bbc1aed2067091bf5ac6c1f5e6966707172cdb90
rémi <debuis22re@lycee-ndduroc.com>
2020-11-20 14:43:49 +0100

    méthode GET, POST, PUT et DELETE pour la table demande_transports

---
+++ app/Http/Controllers/DemandeTransportController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\demandeTransport;
+
+class DemandeTransportController extends Controller
+{
+    function list($id=null)
+    {
+        return $id?DemandeTransport::find($id):DemandeTransport::all();
+    }
+
+    function add(Request $req)
+    {
+        $demandeTransport = new DemandeTransport();
+        $demandeTransport->client_id = $req->client_id;
+        $demandeTransport->villeDepart = $req->villeDepart;
+        $demandeTransport->villeArrivee = $req->villeArrivee;
+        $demandeTransport->distance = $req->distance;
+        $demandeTransport->duree = $req->duree;
+        $demandeTransport->typeMarchandise_id = $req->typrMarchandise_id;
+        $demandeTransport->volume = $req->volume;
+        $demandeTransport->poids = $req->poids;
+        $demandeTransport->dateDemande = $req->dateDemande;
+        $demandeTransport->dateLimite = $req->dateLimite;
+        $demandeTransport->CA = $req->CA;
+        $result = $demandeTransport->save();
+        if ($result)
+        {
+            return ["Resultat" => "Les données ont été enregistrées"];
+        }
+        else
+        {
+            return ["Resultat" => "Opération refusée"];
+        }
+    }
+
+
+    function edit(Request $req)
+    {
+        $demandeTransport=DemandeTransport::find($req->id);
+        $demandeTransport->client_id = $req->client_id;
+        $demandeTransport->villeDepart = $req->villeDepart;
+        $demandeTransport->villeArrivee = $req->villeArrivee;
+        $demandeTransport->distance = $req->distance;
+        $demandeTransport->duree = $req->duree;
+        $demandeTransport->typeMarchandise_id = $req->typrMarchandise_id;
+        $demandeTransport->volume = $req->volume;
+        $demandeTransport->poids = $req->poids;
+        $demandeTransport->dateDemande = $req->dateDemande;
+        $demandeTransport->dateLimite = $req->dateLimite;
+        $demandeTransport->CA = $req->CA;
+        $result=$demandeTransport->save();
+        if($result)
+        {
+            return ["Resultat"=>"Données modifiées"];
+        }
+        else
+        {
+            return ["Resultat"=>"Modification échouée"];
+        }
+
+    }
+
+    function delete($id)
+    {
+        $demandeTransport = DemandeTransport::find($id);
+        $result=$demandeTransport->delete();
+        if($result)
+        {
+            return ["Resultat"=>"Données supprimées"];
+        }
+        else
+        {
+            return ["Resultat"=>"suppression echouée"];
+        }
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ClientsController;
 use App\Http\Controllers\TrajetController;
 use App\Http\Controllers\ConducteursController;
 use App\Http\Controllers\VehiculesController;
+use App\Http\Controllers\DemandeTransportController;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -30,22 +31,27 @@ route::get("clients/{id?}",[ClientsController::class,'ClientsAll']);
 route::get("trajet/{id?}",[TrajetController::class,'trajet']);
 route::get("conducteurs/{id?}",[ConducteursController::class,'conducteurs']);
 route::get("vehicules/{id?}",[VehiculesController::class,'vehicules']);
+route::get("demande/{id?}",[DemandeTransportController::class,'demandeTransport']);
 
 route::post("devis/add",[DevisController::class,'add']);
 route::post("clients/add",[ClientsController::class,'add']);
 route::post("trajet/add",[TrajetController::class,'add']);
 route::post("conducteurs/add",[ConducteursController::class,'add']);
 route::post("vehicules/add",[VehiculesController::class,'add']);
+route::post("demande/add",[DemandeTransportController::class,'add']);
+
 
 route::delete("devis/delete/{id}",[DevisController::class,'delete']);
 route::delete("clients/delete/{id}",[ClientsController::class,'delete']);
 route::delete("trajet/delete/{id}",[TrajetController::class,'delete']);
 route::delete("conducteurs/delete/{id}",[ConducteursController::class,'delete']);
 route::delete("vehicules/delete/{id}",[VehiculesController::class,'delete']);
+route::delete("demande/delete/{id}",[DemandeTransportController::class,'delete']);
 
 route::put("devis/edit",[DevisController::class,'edit']);
 route::put("clients/edit",[ClientsController::class,'edit']);
 route::put("trajet/edit",[TrajetController::class,'edit']);
 route::put("conducteurs/edit",[ConducteursController::class,'edit']);
 route::put("vehicules/edit",[VehiculesController::class,'edit']);
+route::put("demande/edit",[DemandeTransportController::class,'edit']);
 
